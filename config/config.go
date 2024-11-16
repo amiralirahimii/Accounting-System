0d378f336dc2d649782dcba7e5e447529d71d4d6
amiralirahimii <amiralirahimi82@gmail.com>
2024-11-16 15:42:28 +0330

    feat: add connection to database

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// InitDB initializes the database connection
+func InitDB() {
+	dsn := "host=localhost user=postgres password=1382138ceE@ dbname=accountingsystem port=5432 sslmode=disable"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	log.Println("Database connection established")
+}
