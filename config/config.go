c5e3caba7a213198ef0b67a02ade1658037d6263
amiralirahimii <amiralirahimi82@gmail.com>
2024-11-16 15:49:55 +0330

    feat: move sensetive data for database to .env

--- config/config.go
+++ config/config.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,8 +14,25 @@ var DB *gorm.DB
 
 // InitDB initializes the database connection
 func InitDB() {
-	dsn := "host=localhost user=postgres password=1382138ceE@ dbname=accountingsystem port=5432 sslmode=disable"
-	var err error
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Get environment variables
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslMode := os.Getenv("DB_SSLMODE")
+
+	// Construct the DSN
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbName, port, sslMode)
+
+	// Connect to the database
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module accountingsystem
 go 1.23.0
 
 require (
+	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.12
 )
