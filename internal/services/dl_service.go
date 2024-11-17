647838b4f8bf9fb2eb07b34016d1b470eb8f4cd8
amiralirahimii <amiralirahimi82@gmail.com>
2024-11-17 18:33:03 +0330

    fix: fix 1 bug of dl_service

--- internal/services/dl_service.go
+++ internal/services/dl_service.go
@@ -56,7 +56,8 @@ func (s *DLService) UpdateDL(req *dl.UpdateRequest) (*models.DL, error) {
 	}
 
 	var targerDL models.DL
-	if err := db.DB.Find(&targerDL, req.ID).Error; err != nil {
+	if err := db.DB.Where("id = ?", req.ID).First(&targerDL).Error; err != nil {
+		println("SALAAAAAAAAm")
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, constants.ErrDLNotFound
 		}
