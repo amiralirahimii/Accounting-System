8e7c0447ac19dee24db2a72c9a43a5471b6540e4
amiralirahimii <amiralirahimi82@gmail.com>
2024-11-17 20:26:44 +0330

    fix: change is_dl name to has_dl

--- internal/models/sl.go
+++ internal/models/sl.go
@@ -6,7 +6,7 @@ type SL struct {
 	ID         int       `gorm:"primaryKey"`
 	Code       string    `gorm:"size:64;not null;unique"`
 	Title      string    `gorm:"size:64;not null;unique"`
-	IsDL       bool      `gorm:"not null"`
+	HasDL      bool      `gorm:"not null"`
 	RowVersion int       `gorm:"not null;default:0"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"`
 	UpdatedAt  time.Time `gorm:"autoUpdateTime"`
