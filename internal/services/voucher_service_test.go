ca94a7de8154ad346e942d9e2d319e958fa1a094
amiralirahimii <amiralirahimi82@gmail.com>
2024-11-18 17:42:15 +0330

    feat: complete tests for create-voucher endpoint

--- internal/services/voucher_service_test.go
+++ internal/services/voucher_service_test.go
@@ -121,7 +121,7 @@ func Test_CreateVoucher_ReturnsErrNumberEmptyOrTooLong_WithTooLongNumber(t *test
 	assert.Nil(t, voucher)
 }
 
-func Test_CreateVoucher_Fails_WithLessThanTwoVoucherItems(t *testing.T) {
+func Test_CreateVoucher_ReturnsErrVoucherItemsCountOutOfRange_WithLessThanTwoVoucherItems(t *testing.T) {
 	items := []voucher.VoucherItemInsertRequest{
 		{
 			SLID:   1,
@@ -142,7 +142,7 @@ func Test_CreateVoucher_Fails_WithLessThanTwoVoucherItems(t *testing.T) {
 	assert.Nil(t, voucher)
 }
 
-func Test_CreateVoucher_Fails_WithMoreThan500VoucherItems(t *testing.T) {
+func Test_CreateVoucher_ReturnsErrVoucherItemsCountOutOfRange_WithMoreThan500VoucherItems(t *testing.T) {
 	var items []voucher.VoucherItemInsertRequest
 	for i := 0; i < 501; i++ {
 		items = append(items, voucher.VoucherItemInsertRequest{
@@ -164,3 +164,264 @@ func Test_CreateVoucher_Fails_WithMoreThan500VoucherItems(t *testing.T) {
 	assert.Nil(t, voucher)
 }
 
+func Test_CreateVoucher_ReturnsErrDebitOrCreditInvalid_BothDebitAndCreditNonZero(t *testing.T) {
+	items := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   1,
+			DLID:   nil,
+			Debit:  100,
+			Credit: 50,
+		},
+		{
+			SLID:   2,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucher, err := voucherService.CreateVoucher(req)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDebitOrCreditInvalid)
+	assert.Nil(t, voucher)
+}
+
+func Test_CreateVoucher_ReturnsErrDebitOrCreditInvalid_BothDebitAndCreditZero(t *testing.T) {
+	items := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   1,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 0,
+		},
+		{
+			SLID:   2,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucher, err := voucherService.CreateVoucher(req)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDebitOrCreditInvalid)
+	assert.Nil(t, voucher)
+}
+
+func Test_CreateVoucher_ReturnsErrDebitCreditMismatch_WithMismatchedDebitCreditSum(t *testing.T) {
+	items := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   1,
+			DLID:   nil,
+			Debit:  100,
+			Credit: 0,
+		},
+		{
+			SLID:   2,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 50,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucher, err := voucherService.CreateVoucher(req)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDebitCreditMismatch)
+	assert.Nil(t, voucher)
+}
+
+func Test_CreateVoucher_ReturnsErrVoucherNumberExists_WithExistingVoucherNumber(t *testing.T) {
+	slWithDL, err := createRandomSLInDB(true)
+	require.Nil(t, err)
+
+	slWithoutDL, err := createRandomSLInDB(false)
+	require.Nil(t, err)
+
+	dl, err := createRandomDLInDB()
+	require.Nil(t, err)
+
+	initialItems := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   slWithDL.ID,
+			DLID:   &dl.ID,
+			Debit:  100,
+			Credit: 0,
+		},
+		{
+			SLID:   slWithoutDL.ID,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	initialReq := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: initialItems,
+	}
+
+	_, err = voucherService.CreateVoucher(initialReq)
+	require.Nil(t, err)
+
+	newItems := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   1,
+			DLID:   nil,
+			Debit:  200,
+			Credit: 0,
+		},
+		{
+			SLID:   2,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 200,
+		},
+	}
+
+	newReq := &voucher.InsertRequest{
+		Number:       initialReq.Number,
+		VoucherItems: newItems,
+	}
+
+	voucher, err := voucherService.CreateVoucher(newReq)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrVoucherNumberExists)
+	assert.Nil(t, voucher)
+}
+
+func Test_CreateVoucher_ReturnsErrSLNotFound_WithInvalidSLID(t *testing.T) {
+	items := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   generateRandomInt64(),
+			DLID:   nil,
+			Debit:  100,
+			Credit: 0,
+		},
+		{
+			SLID:   generateRandomInt64(),
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucher, err := voucherService.CreateVoucher(req)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrSLNotFound)
+	assert.Nil(t, voucher)
+}
+
+func Test_CreateVoucher_ReturnsErrDLIDRequired_WhenSLRequiresDLButNoDLProvided(t *testing.T) {
+	slWithDL, err := createRandomSLInDB(true)
+	require.Nil(t, err)
+
+	items := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   slWithDL.ID,
+			DLID:   nil,
+			Debit:  100,
+			Credit: 0,
+		},
+		{
+			SLID:   slWithDL.ID,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucher, err := voucherService.CreateVoucher(req)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDLIDRequired)
+	assert.Nil(t, voucher)
+}
+
+func Test_CreateVoucher_ReturnsErrDLNotAllowed_WhenSLDoesNotRequireDLButDLProvided(t *testing.T) {
+	slWithoutDL, err := createRandomSLInDB(false)
+	require.Nil(t, err)
+
+	dl, err := createRandomDLInDB()
+	require.Nil(t, err)
+
+	items := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   slWithoutDL.ID,
+			DLID:   &dl.ID,
+			Debit:  100,
+			Credit: 0,
+		},
+		{
+			SLID:   slWithoutDL.ID,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucher, err := voucherService.CreateVoucher(req)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDLNotAllowed)
+	assert.Nil(t, voucher)
+}
+
+func Test_CreateVoucher_ReturnsErrDLNotFound_WithInvalidDLID(t *testing.T) {
+	slWithDL, err := createRandomSLInDB(true)
+	require.Nil(t, err)
+
+	invalidDLID := generateRandomInt64()
+
+	items := []voucher.VoucherItemInsertRequest{
+		{
+			SLID:   slWithDL.ID,
+			DLID:   &invalidDLID,
+			Debit:  100,
+			Credit: 0,
+		},
+		{
+			SLID:   slWithDL.ID,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucher, err := voucherService.CreateVoucher(req)
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDLNotFound)
+	assert.Nil(t, voucher)
+}
