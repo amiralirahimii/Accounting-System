e469d113b24a69d22ac1bf19d1f6fb55a9b252c4
amiralirahimii <amiralirahimi82@gmail.com>
2024-11-19 17:05:43 +0330

    feat: complete tests for update-voucher endpoint

--- internal/services/voucher_service_test.go
+++ internal/services/voucher_service_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"accountingsystem/internal/constants"
+	"accountingsystem/internal/dtos"
 	"accountingsystem/internal/requests/voucher"
 	"testing"
 
@@ -414,3 +415,698 @@ func Test_CreateVoucher_ReturnsErrDLNotFound_WithInvalidDLID(t *testing.T) {
 	assert.Nil(t, voucher)
 }
 
+func createRandomVoucher() (*dtos.VoucherDto, error) {
+	slWithDL, err := createRandomSL(&slService, true)
+	if err != nil {
+		return nil, err
+	}
+
+	slWithoutDL, err := createRandomSL(&slService, false)
+	if err != nil {
+		return nil, err
+	}
+
+	dl, err := createRandomDL(&dlService)
+	if err != nil {
+		return nil, err
+	}
+
+	items := []voucher.VoucherItemInsertDetail{
+		{
+			SLID:   slWithDL.ID,
+			DLID:   &dl.ID,
+			Debit:  100,
+			Credit: 0,
+		},
+		{
+			SLID:   slWithoutDL.ID,
+			DLID:   nil,
+			Debit:  0,
+			Credit: 100,
+		},
+	}
+
+	req := &voucher.InsertRequest{
+		Number:       generateRandomString(20),
+		VoucherItems: items,
+	}
+
+	voucherDto, err := voucherService.CreateVoucher(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return voucherDto, nil
+}
+
+func Test_UpdateVoucher_Succeeds_WithValidRequest(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	SLWithDL, err := createRandomSL(&slService, true)
+	require.Nil(t, err)
+
+	SLWithoutDL, err := createRandomSL(&slService, false)
+	require.Nil(t, err)
+
+	DL, err := createRandomDL(&dlService)
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   SLWithDL.ID,
+				DLID:   &DL.ID,
+				Debit:  1000,
+				Credit: 0,
+			},
+			{
+				SLID:   SLWithoutDL.ID,
+				DLID:   nil,
+				Debit:  0,
+				Credit: 500,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{
+			{
+				ID:     voucherDto.VoucherItems[0].ID,
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   &voucherDto.VoucherItems[0].DLID,
+				Debit:  0,
+				Credit: 500,
+			},
+		},
+		Deleted: []int{
+			voucherDto.VoucherItems[1].ID,
+		},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.Nil(t, err)
+	assert.Equal(t, req.Number, voucher.Number)
+}
+
+func Test_UpdateVoucher_ReturnsErrVoucherNotFound_WithNonExistentVoucherID(t *testing.T) {
+	insertReq, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   insertReq.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   insertReq.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  0,
+				Credit: 100,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      generateRandomInt64(),
+		Version: 0,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrVoucherNotFound)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrNumberEmptyOrTooLong_WithEmptyNumber(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  0,
+				Credit: 100,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  "",
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrNumberEmptyOrTooLong)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrNumberEmptyOrTooLong_WithTooLongNumber(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  0,
+				Credit: 100,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(65),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrNumberEmptyOrTooLong)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrVersionOutdated_WithOutdatedRequest(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	SLWithDL, err := createRandomSL(&slService, true)
+	require.Nil(t, err)
+
+	DL, err := createRandomDL(&dlService)
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   SLWithDL.ID,
+				DLID:   &DL.ID,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   SLWithDL.ID,
+				DLID:   &DL.ID,
+				Debit:  0,
+				Credit: 100,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	_, err = voucherService.UpdateVoucher(req)
+
+	require.Nil(t, err)
+
+	updatedVoucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrVersionOutdated)
+	assert.Nil(t, updatedVoucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrSLNotFound_WithInvalidSLInInserts(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   generateRandomInt64(),
+				DLID:   nil,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   generateRandomInt64(),
+				DLID:   nil,
+				Debit:  0,
+				Credit: 100,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrSLNotFound)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrDLIDRequired_WhenSLRequiresDLButNoDLProvidedInInserts(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	slWithDL, err := createRandomSL(&slService, true)
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   slWithDL.ID,
+				DLID:   nil,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   slWithDL.ID,
+				DLID:   nil,
+				Debit:  0,
+				Credit: 100,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDLIDRequired)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrDLNotAllowed_WhenSLDoesNotRequireDLButDLProvidedInInserts(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	slWithoutDL, err := createRandomSL(&slService, false)
+	require.Nil(t, err)
+
+	dl, err := createRandomDL(&dlService)
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   slWithoutDL.ID,
+				DLID:   &dl.ID,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   slWithoutDL.ID,
+				DLID:   &dl.ID,
+				Debit:  0,
+				Credit: 100,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDLNotAllowed)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrDebitOrCreditInvalid_InvalidValuesInInserts(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  100,
+				Credit: 50,
+			},
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  0,
+				Credit: 50,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDebitOrCreditInvalid)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrSLNotFound_WithInvalidSLInUpdates(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Updated: []voucher.VoucherItemUpdateDetail{
+			{
+				ID:     voucherDto.VoucherItems[0].ID,
+				SLID:   generateRandomInt64(),
+				DLID:   nil,
+				Debit:  voucherDto.VoucherItems[0].Debit,
+				Credit: voucherDto.VoucherItems[0].Credit,
+			},
+		},
+		Inserted: []voucher.VoucherItemInsertDetail{},
+		Deleted:  []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrSLNotFound)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrDLIDRequired_WhenSLRequiresDLButNoDLProvidedInUpdates(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	slWithDL, err := createRandomSL(&slService, true)
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Updated: []voucher.VoucherItemUpdateDetail{
+			{
+				ID:     voucherDto.VoucherItems[0].ID,
+				SLID:   slWithDL.ID,
+				DLID:   nil,
+				Debit:  voucherDto.VoucherItems[0].Debit,
+				Credit: voucherDto.VoucherItems[0].Credit,
+			},
+		},
+		Inserted: []voucher.VoucherItemInsertDetail{},
+		Deleted:  []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDLIDRequired)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrDLNotAllowed_WhenSLDoesNotRequireDLButDLProvidedInUpdates(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	slWithoutDL, err := createRandomSL(&slService, false)
+	require.Nil(t, err)
+
+	dl, err := createRandomDL(&dlService)
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Updated: []voucher.VoucherItemUpdateDetail{
+			{
+				ID:     voucherDto.VoucherItems[0].ID,
+				SLID:   slWithoutDL.ID,
+				DLID:   &dl.ID,
+				Debit:  voucherDto.VoucherItems[0].Debit,
+				Credit: voucherDto.VoucherItems[0].Credit,
+			},
+		},
+		Inserted: []voucher.VoucherItemInsertDetail{},
+		Deleted:  []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDLNotAllowed)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrDebitOrCreditInvalid_InvalidValuesInUpdates(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Updated: []voucher.VoucherItemUpdateDetail{
+			{
+				ID:     voucherDto.VoucherItems[0].ID,
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   nil,
+				Debit:  voucherDto.VoucherItems[0].Debit + 50,
+				Credit: voucherDto.VoucherItems[0].Credit + 50,
+			},
+		},
+		Inserted: []voucher.VoucherItemInsertDetail{},
+		Deleted:  []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDebitOrCreditInvalid)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrVoucherItemNotFound_WithInvalidVoucherItemIDInUpdates(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	invalidVoucherItemID := generateRandomInt64()
+
+	items := voucher.VoucherItemsUpdate{
+		Updated: []voucher.VoucherItemUpdateDetail{
+			{
+				ID:     invalidVoucherItemID,
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   &voucherDto.VoucherItems[0].DLID,
+				Debit:  voucherDto.VoucherItems[0].Debit,
+				Credit: voucherDto.VoucherItems[0].Credit,
+			},
+		},
+		Inserted: []voucher.VoucherItemInsertDetail{},
+		Deleted:  []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrVoucherItemNotFound)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrDebitCreditMismatch_WithUnbalancedRequest(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   &voucherDto.VoucherItems[0].DLID,
+				Debit:  100,
+				Credit: 0,
+			},
+			{
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   &voucherDto.VoucherItems[0].DLID,
+				Debit:  100,
+				Credit: 0,
+			},
+		},
+		Updated: []voucher.VoucherItemUpdateDetail{
+			{
+				ID:     voucherDto.VoucherItems[0].ID,
+				SLID:   voucherDto.VoucherItems[0].SLID,
+				DLID:   &voucherDto.VoucherItems[0].DLID,
+				Debit:  0,
+				Credit: 500,
+			},
+		},
+		Deleted: []int{voucherDto.VoucherItems[1].ID},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrDebitCreditMismatch)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrVoucherItemsCountOutOfRange_WhenCountGoesBelowTwo(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: []voucher.VoucherItemInsertDetail{},
+		Updated:  []voucher.VoucherItemUpdateDetail{},
+		Deleted: []int{
+			voucherDto.VoucherItems[0].ID,
+		},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrVoucherItemsCountOutOfRange)
+	assert.Nil(t, voucher)
+}
+
+func Test_UpdateVoucher_ReturnsErrVoucherItemsCountOutOfRange_WhenCountExceedsFiveHundred(t *testing.T) {
+	voucherDto, err := createRandomVoucher()
+	require.Nil(t, err)
+
+	var insertedItems []voucher.VoucherItemInsertDetail
+	for i := 0; i < 498; i++ {
+		insertedItems = append(insertedItems, voucher.VoucherItemInsertDetail{
+			SLID:   voucherDto.VoucherItems[0].SLID,
+			DLID:   nil,
+			Debit:  100,
+			Credit: 0,
+		})
+	}
+	insertedItems = append(insertedItems, voucher.VoucherItemInsertDetail{
+		SLID:   voucherDto.VoucherItems[0].SLID,
+		DLID:   nil,
+		Debit:  0,
+		Credit: 50000,
+	})
+
+	items := voucher.VoucherItemsUpdate{
+		Inserted: insertedItems,
+		Updated:  []voucher.VoucherItemUpdateDetail{},
+		Deleted:  []int{},
+	}
+
+	req := &voucher.UpdateRequest{
+		ID:      voucherDto.ID,
+		Version: voucherDto.RowVersion,
+		Number:  generateRandomString(20),
+		Items:   items,
+	}
+
+	voucher, err := voucherService.UpdateVoucher(req)
+
+	require.NotNil(t, err)
+	assert.ErrorIs(t, err, constants.ErrVoucherItemsCountOutOfRange)
+	assert.Nil(t, voucher)
+}
