da2202f2f126ca99fd4ce65fa5567f2052b0f7be
amiralirahimii <amiralirahimi82@gmail.com>
2024-11-22 16:40:23 +0330

    refactor: refactor dl-service. remove code duplications and break into meaningful functions

--- internal/services/dl_service.go
+++ internal/services/dl_service.go
@@ -16,24 +16,8 @@ import (
 type DLService struct{}
 
 func (s *DLService) CreateDL(req *dl.InsertRequest) (*dtos.DLDto, error) {
-	if req.Code == "" || len(req.Code) > 64 {
-		return nil, constants.ErrCodeEmptyOrTooLong
-	}
-	if req.Title == "" || len(req.Title) > 64 {
-		return nil, constants.ErrTitleEmptyOrTooLong
-	}
-
-	var existingDL models.DL
-	if err := db.DB.Where("code = ? OR title = ?", req.Code, req.Title).First(&existingDL).Error; err == nil {
-		if existingDL.Code == req.Code {
-			return nil, constants.ErrCodeAlreadyExists
-		}
-		if existingDL.Title == req.Title {
-			return nil, constants.ErrTitleAlreadyExists
-		}
-	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
-		log.Printf("unexpected error while checking for duplicates: %v", err)
-		return nil, constants.ErrUnexpectedError
+	if err := s.validateDLInsertRequest(req); err != nil {
+		return nil, err
 	}
 
 	dl := models.DL{
@@ -49,67 +33,114 @@ func (s *DLService) CreateDL(req *dl.InsertRequest) (*dtos.DLDto, error) {
 	return mappers.ToDLDto(&dl), nil
 }
 
-func (s *DLService) UpdateDL(req *dl.UpdateRequest) (*dtos.DLDto, error) {
-	if req.Code == "" || len(req.Code) > 64 {
-		return nil, constants.ErrCodeEmptyOrTooLong
+func (s *DLService) validateDLInsertRequest(req *dl.InsertRequest) error {
+	if err := s.validateCodeAndTitleLength(req.Code, req.Title); err != nil {
+		return err
 	}
-	if req.Title == "" || len(req.Title) > 64 {
-		return nil, constants.ErrTitleEmptyOrTooLong
+	if err := s.validateCodeAndTitleUnique(req.Code, req.Title); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *DLService) validateCodeAndTitleUnique(code string, title string) error {
+	var existingDL models.DL
+	if err := db.DB.Where("code = ? OR title = ?", code, title).First(&existingDL).Error; err == nil {
+		if existingDL.Code == code {
+			return constants.ErrCodeAlreadyExists
+		}
+		if existingDL.Title == title {
+			return constants.ErrTitleAlreadyExists
+		}
+	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+		log.Printf("unexpected error while checking for duplicates: %v", err)
+		return constants.ErrUnexpectedError
+	}
+	return nil
+}
+
+func (s *DLService) validateCodeAndTitleLength(code string, title string) error {
+	if code == "" || len(code) > 64 {
+		return constants.ErrCodeEmptyOrTooLong
+	}
+	if title == "" || len(title) > 64 {
+		return constants.ErrTitleEmptyOrTooLong
+	}
+	return nil
+}
+
+func (s *DLService) UpdateDL(req *dl.UpdateRequest) (*dtos.DLDto, error) {
+	targetDL, err := s.validateDLUpdateRequest(req)
+	if err != nil {
+		return nil, err
 	}
 
+	targetDL.Code = req.Code
+	targetDL.Title = req.Title
+	targetDL.RowVersion++
+	if err := db.DB.Save(targetDL).Error; err != nil {
+		return nil, err
+	}
+
+	return mappers.ToDLDto(targetDL), nil
+}
+
+func (s *DLService) validateDLUpdateRequest(req *dl.UpdateRequest) (*models.DL, error) {
+	if err := s.validateCodeAndTitleLength(req.Code, req.Title); err != nil {
+		return nil, err
+	}
+	targetDL, err := s.validateDLExists(req.ID)
+	if err != nil {
+		return nil, err
+	}
+	if err := s.validateVersion(req.Version, targetDL.RowVersion); err != nil {
+		return nil, err
+	}
+	if err := s.validateCodeAndTitleUniqueWithDifferentId(req.Code, req.Title, req.ID); err != nil {
+		return nil, err
+	}
+	return targetDL, nil
+}
+
+func (s *DLService) validateCodeAndTitleUniqueWithDifferentId(code string, title string, id int) error {
+	var existingDL models.DL
+	if err := db.DB.Where("(code = ? OR title = ?) AND id != ?", code, title, id).First(&existingDL).Error; err == nil {
+		if existingDL.Code == code {
+			return constants.ErrCodeAlreadyExists
+		}
+		if existingDL.Title == title {
+			return constants.ErrTitleAlreadyExists
+		}
+	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+		log.Printf("unexpected error while checking for duplicates: %v", err)
+		return constants.ErrUnexpectedError
+	}
+	return nil
+}
+
+func (s *DLService) validateVersion(reqVersion int, targetVersion int) error {
+	if reqVersion != targetVersion {
+		return constants.ErrVersionOutdated
+	}
+	return nil
+}
+
+func (s *DLService) validateDLExists(id int) (*models.DL, error) {
 	var targetDL models.DL
-	if err := db.DB.Where("id = ?", req.ID).First(&targetDL).Error; err != nil {
+	if err := db.DB.Where("id = ?", id).First(&targetDL).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, constants.ErrDLNotFound
 		}
 		log.Printf("unexpected error while finding DL: %v", err)
 		return nil, constants.ErrUnexpectedError
 	}
-
-	if targetDL.RowVersion != req.Version {
-		return nil, constants.ErrVersionOutdated
-	}
-
-	var existingDL models.DL
-	if err := db.DB.Where("(code = ? OR title = ?) AND id != ?", req.Code, req.Title, req.ID).First(&existingDL).Error; err == nil {
-		if existingDL.Code == req.Code {
-			return nil, constants.ErrCodeAlreadyExists
-		}
-		if existingDL.Title == req.Title {
-			return nil, constants.ErrTitleAlreadyExists
-		}
-	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
-		log.Printf("unexpected error while checking for duplicates: %v", err)
-		return nil, constants.ErrUnexpectedError
-	}
-
-	targetDL.Code = req.Code
-	targetDL.Title = req.Title
-	targetDL.RowVersion++
-	if err := db.DB.Save(&targetDL).Error; err != nil {
-		return nil, err
-	}
-
-	return mappers.ToDLDto(&targetDL), nil
+	return &targetDL, nil
 }
 
 func (s *DLService) DeleteDL(req *dl.DeleteRequest) error {
-	var targetDL models.DL
-	if err := db.DB.Where("id = ?", req.ID).First(&targetDL).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return constants.ErrDLNotFound
-		}
-		log.Printf("unexpected error while finding DL: %v", err)
-		return constants.ErrUnexpectedError
-	}
-
-	if targetDL.RowVersion != req.Version {
-		return constants.ErrVersionOutdated
-	}
-
-	var VoucherItemRefrencingThisDL models.VoucherItem
-	if err := db.DB.Where("dl_id = ?", targetDL.ID).First(&VoucherItemRefrencingThisDL).Error; err == nil {
-		return constants.ErrThereIsRefrenceToDL
+	targetDL, err := s.validateDLDeleteRequest(req)
+	if err != nil {
+		return err
 	}
 
 	if err := db.DB.Delete(&targetDL).Error; err != nil {
@@ -120,15 +151,41 @@ func (s *DLService) DeleteDL(req *dl.DeleteRequest) error {
 	return nil
 }
 
+func (s *DLService) validateDLDeleteRequest(req *dl.DeleteRequest) (*models.DL, error) {
+	targetDL, err := s.validateDLExists(req.ID)
+	if err != nil {
+		return nil, err
+	}
+	if err := s.validateVersion(req.Version, targetDL.RowVersion); err != nil {
+		return nil, err
+	}
+	if err := s.validateDLHasNoRefrences(req.ID); err != nil {
+		return nil, err
+	}
+	return targetDL, nil
+}
+
+func (s *DLService) validateDLHasNoRefrences(id int) error {
+	var VoucherItemRefrencingThisDL models.VoucherItem
+	if err := db.DB.Where("dl_id = ?", id).First(&VoucherItemRefrencingThisDL).Error; err == nil {
+		return constants.ErrThereIsRefrenceToDL
+	}
+	return nil
+}
+
 func (s *DLService) GetDL(req *dl.GetRequest) (*dtos.DLDto, error) {
-	var targetDL models.DL
-	if err := db.DB.Where("id = ?", req.ID).First(&targetDL).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, constants.ErrDLNotFound
-		}
-		log.Printf("unexpected error while finding DL: %v", err)
-		return nil, constants.ErrUnexpectedError
+	targetDL, err := s.validateDLGetRequest(req)
+	if err != nil {
+		return nil, err
 	}
 
-	return mappers.ToDLDto(&targetDL), nil
+	return mappers.ToDLDto(targetDL), nil
+}
+
+func (s *DLService) validateDLGetRequest(req *dl.GetRequest) (*models.DL, error) {
+	targetDL, err := s.validateDLExists(req.ID)
+	if err != nil {
+		return nil, err
+	}
+	return targetDL, nil
 }
